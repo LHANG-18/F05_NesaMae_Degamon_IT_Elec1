f924dc5b121b347eba2a3375da35ba2d9aeb688e
LHANG-18 <nesamaeadegamon18@gmail.com>
2025-09-25 13:43:24 +0800

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import ChatScreen from './ChatScreen';
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import CommentSection from './CommentSection'
+import ChatBubble from './ChatSection'
 
 export default function App() {
   return (
