128a3b82bc8b5c14efd2509f68a02d6fc5b1e7ee
LHANG-18 <nesamaeadegamon18@gmail.com>
2025-09-25 13:48:58 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,1 @@
-import ChatScreen from './ChatScreen';
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-import CommentSection from './CommentSection'
 
-export default function App() {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      
-      <CommentSection />
-      <ChatScreen />
- 
-    </SafeAreaView>
-  );
-}
